ce7a438e3dfbd122044a329f2880813230aa7e2b
Eduardo Serrano <eserrano@Eduardos-MacBook-Air.local>
2022-06-16 18:32:22 -0500

    Added deactivated cards logic and view

--- index.js
+++ index.js
@@ -1,61 +1,63 @@
-const express = require('express');
-const axios = require('axios');
-const nodemailer = require('nodemailer');
+const express = require("express");
+const axios = require("axios");
+const nodemailer = require("nodemailer");
+const path = require("path");
 
-const websiteRouter = require('./routers/website.router');
+const websiteRouter = require("./routers/website.router");
 
-require('dotenv/config');
+require("dotenv/config");
 
 const app = express();
 
-app.set('view engine', 'ejs');
-app.use('/public', express.static(__dirname + '/public'));
-app.use('/', websiteRouter);
+app.set("view engine", "ejs");
+app.use("/public", express.static(__dirname + "/public"));
+app.use("/", websiteRouter);
 
-app.get('/:id', (req, res) => {
-
-  axios(`${process.env.API_URL}/${req.params.id}?api_key=${process.env.API_KEY}`)
-    .then(response => {
-
+app.get("/:id", (req, res) => {
+  axios(
+    `${process.env.API_URL}/${req.params.id}?api_key=${process.env.API_KEY}`
+  )
+    .then((response) => {
       const user = response.data;
       try {
-        res.render(user.cardSettings.skin, { user: user })
+        if (user.active) {
+          res.render(user.cardSettings.skin, { user: user });
+        } else {
+          res.sendFile(path.join(__dirname, "/views/misc/deactivated.html"));
+        }
       } catch {
-        return
+        return;
       }
     })
-    .catch(error => {
-      if (error.response)
-        res.sendStatus(error.response.status)
-    })
-})
+    .catch((error) => {
+      if (error.response) res.sendStatus(error.response.status);
+    });
+});
 
-app.get('/:id/join', (req, res) => {
-
-  axios(`${process.env.API_URL}/${req.params.id}?api_key=${process.env.API_KEY}`)
-    .then(response => {
-
+app.get("/:id/join", (req, res) => {
+  axios(
+    `${process.env.API_URL}/${req.params.id}?api_key=${process.env.API_KEY}`
+  )
+    .then((response) => {
       const user = response.data;
 
-      if (user.role == 'coordinador' || user.role == 'gerente') {
-        res.render('ciudadmaderas/join.ejs', { user: user });
+      if (user.role == "coordinador" || user.role == "gerente") {
+        res.render("ciudadmaderas/join.ejs", { user: user });
       } else {
-        res.status(405).json({ error: 'No permission' });
+        res.status(405).json({ error: "No permission" });
       }
     })
-    .catch(error => {
-      if (error.response)
-        res.sendStatus(error.response.status)
-    })
-})
+    .catch((error) => {
+      if (error.response) res.sendStatus(error.response.status);
+    });
+});
 
-app.post('/:id/submit', (req, res) => {
-
-  axios(`${process.env.API_URL}/${req.params.id}?api_key=${process.env.API_KEY}`)
-    .then(response => {
-
+app.post("/:id/submit", (req, res) => {
+  axios(
+    `${process.env.API_URL}/${req.params.id}?api_key=${process.env.API_KEY}`
+  )
+    .then((response) => {
       async function main() {
-
         let transporter = nodemailer.createTransport({
           host: "mail.inteminer.com",
           port: 26,
@@ -65,12 +67,12 @@ app.post('/:id/submit', (req, res) => {
             pass: "Inteminer2022*",
           },
           tls: {
-            rejectUnauthorized: false
-          }
+            rejectUnauthorized: false,
+          },
         });
 
         let info = await transporter.sendMail({
-          from: 'recluta@inteminer.com',
+          from: "recluta@inteminer.com",
           to: user.email,
           subject: "Nueva Aplicación Recibida",
           html: `
@@ -79,21 +81,20 @@ app.post('/:id/submit', (req, res) => {
                   <p>Email: ${req.body.email}</p>
                   <p>Teléfono: ${req.body.phone}</p>
                   <p>Mensaje: ${req.body.message}</p>
-                    `
+                    `,
         });
 
         console.log("Message sent: %s", info.messageId);
       }
 
       main().catch(console.error);
-      res.render('ciudadmaderas/submitted.ejs', { user: user });
+      res.render("ciudadmaderas/submitted.ejs", { user: user });
     })
-    .catch(error => {
-      if (error.response)
-        res.sendStatus(error.response.status)
-    })
-})
+    .catch((error) => {
+      if (error.response) res.sendStatus(error.response.status);
+    });
+});
 
 app.listen(5500, () => {
-  console.log('App running at http://localhost:5500')
-})
+  console.log("App running at http://localhost:5500");
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-  darkMode: 'class',
-  content: ["./views/**/*.ejs"],
+  darkMode: "class",
+  content: ["./views/**/*.{ejs,html}"],
   theme: {
     extend: {
       keyframes: {
         slidein: {
-          '0%': { bottom: '-100%' },
-          '100%': { bottom: '0' }
+          "0%": { bottom: "-100%" },
+          "100%": { bottom: "0" },
         },
         slideout: {
-          '0%': { bottom: '0' },
-          '100%': { bottom: '-100%' }
-        }
-      }
+          "0%": { bottom: "0" },
+          "100%": { bottom: "-100%" },
+        },
+      },
     },
   },
   plugins: [],
-}
+};
